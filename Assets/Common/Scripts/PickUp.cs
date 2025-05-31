063bf1e1b6b8c82454b12b3e3d80ca9907b0afe7
Crazysheeptobman <kenobi1toby@gmail.com>
2025-05-31 19:32:33 +1000

    Upgraded robot

--- Assets/Common/Scripts/PickUp.cs
+++ Assets/Common/Scripts/PickUp.cs
@@ -2,27 +2,39 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-
     GameManager gameManager;
+    private AudioSource pickupAudio;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        pickupAudio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider otherObject)
     {
-        if(otherObject.transform.tag == "Player")
+        if (otherObject.CompareTag("Player"))
         {
             gameManager.currentPickups += 1;
-            Destroy(this.gameObject);
+
+            // Play the pickup sound
+            if (pickupAudio != null)
+            {
+                pickupAudio.Play();
+
+                // Disable visuals and collider immediately (optional but cleaner)
+                GetComponent<Collider>().enabled = false;
+                foreach (Renderer r in GetComponentsInChildren<Renderer>())
+                    r.enabled = false;
+
+                // Destroy the object after the clip finishes playing
+                Destroy(gameObject, pickupAudio.clip.length);
+            }
+            else
+            {
+                Destroy(gameObject); // Fallback in case AudioSource is missing
+            }
         }
     }
 }
